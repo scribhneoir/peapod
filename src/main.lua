090ab869e17031f6308cd1e82e860ed5ded7634b
Josiah McCracken <scribhneoir21@gmail.com>
2026-02-15 19:27:23 -0500

    chore: remove commented test code

--- src/main.lua
+++ src/main.lua
@@ -15,15 +15,7 @@ local ui <const> = UI
 
 -- setup
 playdate.display.setRefreshRate(0) -- uncapped framerate
--- local function setup()
---     local file = playdate.file.open("distractible.txt")
---     assert(file, "Failed to open distractible.txt")
---     local xml = file:read(playdate.file.getSize("distractible.txt"))
---     file:close()
---     local dat = string.gsub(xml, "<br>", "")
---     local items = ParseXML(dat)
---     print(items)
--- end
+
 local lastFrameTime = playdate.getCurrentTimeMilliseconds()
 DeltaTime = 0
 
